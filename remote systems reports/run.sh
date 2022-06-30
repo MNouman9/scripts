8a0d2f0b290b0372eb0d4c1cecddeac638eacdc7
Muhammad Nouman Nadeem <m.nouman.nadeem@gmail.com>
2022-06-30 13:46:51 +0500

    Removed sensitive info

--- remote systems reports/run.sh
+++ remote systems reports/run.sh
@@ -9,16 +9,16 @@ if [ "$#" -ne 1 ]; then
 fi
 HOST_FILE=$1
 REPORTS_DIR=$(date +\%d_%B_%Y)
-mkdir -p /home/nouman/Desktop/reports/$REPORTS_DIR
+mkdir -p /home/$USER/Desktop/reports/$REPORTS_DIR
 
 while IFS="" read -r line || [ -n "$line" ]; do
     user=$(echo $line | cut -f1 -d " ")
     ip_address=$(echo $line | cut -f2 -d " ")
     backup_dir=$(echo $line | cut -f3 -d " ")
-    ssh -i /home/nouman/Desktop/creds/ssh_keys/appelit/noc247ne_ssh $user@$ip_address 'bash -s'< /home/nouman/Documents/scripts/system_report.sh $user $ip_address $backup_dir
+    ssh -i [ssh_key] $user@$ip_address 'bash -s'< /home/$USER/Documents/scripts/system_report.sh $user $ip_address $backup_dir
 
-    scp -i /home/nouman/Desktop/creds/ssh_keys/appelit/noc247ne_ssh $user@$ip_address:/home/$user/*system_report* /home/nouman/Desktop/reports/$REPORTS_DIR 2>/dev/null
-    ssh -i /home/nouman/Desktop/creds/ssh_keys/appelit/noc247ne_ssh $user@$ip_address 'bash -s'< /home/nouman/Documents/scripts/remove.sh $user $ip_address
+    scp -i [ssh_key] $user@$ip_address:/home/$user/*system_report* /home/$USER/Desktop/reports/$REPORTS_DIR 2>/dev/null
+    ssh -i [ssh_key] $user@$ip_address 'bash -s'< /home/$USER/Documents/scripts/remove.sh $user $ip_address
 
 
 done < $HOST_FILE
